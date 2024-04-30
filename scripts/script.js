30021784e891e640a1a74587b2d04c06a027b810
Danyabah <tedrikyt881@gmail.com>
2024-04-30 07:59:04 +0300

    html&css

--- scripts/script.js
+++ scripts/script.js
@@ -1,5 +1,12 @@
 const filters = document.querySelectorAll(".sol__filter ");
 const solItems = document.querySelectorAll(`.sol__item`);
+const caseBtns = document.querySelectorAll(".case__btn");
+const comboxTop = document.querySelector(".combobox__top");
+const combox = document.querySelector(".combobox");
+
+comboxTop.addEventListener("click", () => {
+  combox.classList.toggle("combobox-active");
+});
 handleCategory(filters[0]);
 
 filters.forEach((item) => {
@@ -9,6 +16,10 @@ filters.forEach((item) => {
     });
     handleCategory(item);
   };
+});
+
+caseBtns.forEach((item) => {
+  item.onclick = handleCaseActive;
 });
 
 function handleCategory(item) {
@@ -22,4 +33,9 @@ function handleCategory(item) {
       item.classList.remove("sol__item-active");
     }
   });
+}
+
+function handleCaseActive(event) {
+  const item = event.target.closest(".case__item");
+  item.classList.toggle("case__item-active");
 }
